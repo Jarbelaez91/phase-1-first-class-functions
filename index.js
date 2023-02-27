6d9f68a82835136a1940ba8306e665114cb51346
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-02-27 17:20:48 -0500

    completed assignemnt

---
+++ index.js
@@ -0,0 +1,27 @@
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+    return function (fare) {
+        return multiplier * fare
+    }
+}
+
+//const fareDoubler = function (fare) {
+  //  return createFareMultiplier(2)
+
+const fareDoubler = createFareMultiplier(2)
+//fareDoubler(20)
+
+const fairTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, driverGroup) {
+    return driverGroup(drivers)
+}
